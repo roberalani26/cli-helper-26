4e837bc44929199fefb21f66cae63ba7ad9c5429
rober alani <robermz28121990@hotmail.com>
2026-09-23 09:07:06 +0200

    rotational logging setup with dual handlers

--- logger.py
+++ logger.py
@@ -1,32 +1,30 @@
 import logging
 from logging.handlers import RotatingFileHandler
-import sys
+import os
 
-class LoggerSetup:
-    def __init__(self, name='cli-helper-26', log_file='app.log'):
-        self.logger = logging.getLogger(name)
-        self.logger.setLevel(logging.DEBUG)
-        self.formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        self.setup_handlers(log_file)
+def get_logger(name='cli-helper-26', log_file='app.log'):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+    
+    if not logger.handlers:
+        formatter = logging.Formatter(
+            '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+            datefmt='%Y-%m-%d %H:%M:%S'
+        )
+        
+        file_handler = RotatingFileHandler(
+            log_file, 
+            maxBytes=1024 * 1024 * 5, 
+            backupCount=3
+        )
+        file_handler.setFormatter(formatter)
+        
+        console_handler = logging.StreamHandler()
+        console_handler.setFormatter(formatter)
+        
+        logger.addHandler(file_handler)
+        logger.addHandler(console_handler)
+        
+    return logger
 
-    def setup_handlers(self, log_file):
-        if not self.logger.handlers:
-            file_handler = RotatingFileHandler(
-                log_file, maxBytes=1048576, backupCount=5
-            )
-            file_handler.setFormatter(self.formatter)
-            self.logger.addHandler(file_handler)
-
-            console_handler = logging.StreamHandler(sys.stdout)
-            console_handler.setFormatter(self.formatter)
-            self.logger.addHandler(console_handler)
-
-    def get_logger(self):
-        return self.logger
-
-def get_app_logger():
-    return LoggerSetup().get_logger()
-
-if __name__ == '__main__':
-    log = get_app_logger()
-    log.info('logger initialization successful')
+log = get_logger()
